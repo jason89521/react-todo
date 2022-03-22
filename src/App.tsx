da423e3ef5ab5dd8bee374635cc2a3de885420fc
Xuan <97ssps30212@gmail.com>
2022-03-22 23:41:34 +0800

    Complete ListItem appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 function App() {
   return (
-    <div className="min-h-screen bg-mobile-light bg-no-repeat py-12 px-6">
+    <div className="min-h-screen bg-mobile-light bg-contain bg-no-repeat py-12 px-6">
       <Header />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
     </div>
   );
 }
---
+++ src/components/ListItem.tsx
@@ -0,0 +1,17 @@
+import iconCross from '../images/icon-cross.svg';
+
+type Props = {};
+
+const ListItem = ({}: Props) => {
+  return (
+    <li className="flex items-center gap-4 border-b border-b-gray-300 bg-white px-4 py-3">
+      <button className="h-5 w-5 rounded-full border border-gray-300"></button>
+      <span className="mt-1 flex-1 text-sm text-slate-600">Create a new todo...</span>
+      <button className="h-3 w-3">
+        <img src={iconCross} alt="delete task" />
+      </button>
+    </li>
+  );
+};
+
+export default ListItem;
