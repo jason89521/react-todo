6bdde209d45ff31d84581ae1a8c63e79bce2735e
Xuan <97ssps30212@gmail.com>
2022-03-22 22:40:11 +0800

    Complete Header appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from './components/Header';
+
 function App() {
-  return <div className="min-h-screen bg-mobile-light bg-no-repeat"></div>;
+  return (
+    <div className="min-h-screen bg-mobile-light bg-no-repeat py-12 px-6">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import iconMoon from '../images/icon-moon.svg';
+
+const Header = () => {
+  return (
+    <header className="flex items-baseline justify-between">
+      <h1 className="text-3xl font-bold uppercase tracking-[10px] text-white">Todo</h1>
+      <button>
+        <img src={iconMoon} alt="change to moon theme" />
+      </button>
+    </header>
+  );
+};
+
+export default Header;
