30f1fd2695892c212dfab6721044000e0dd4ee97
Xuan <97ssps30212@gmail.com>
2022-03-23 16:12:01 +0800

    Complete mobile app appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,60 @@
 import Header from './components/Header';
+import Form from './components/Form';
 import ListItem from './components/ListItem';
+
+const Todos = [
+  {
+    isCompleted: false,
+    title: 'Create a new todos...',
+  },
+  {
+    isCompleted: false,
+    title: 'Create a new todos...',
+  },
+  {
+    isCompleted: false,
+    title: 'Create a new todos...',
+  },
+  {
+    isCompleted: false,
+    title: 'Create a new todos...',
+  },
+  {
+    isCompleted: false,
+    title: 'Create a new todos...',
+  },
+  {
+    isCompleted: false,
+    title: 'Create a new todos...',
+  },
+];
 
 function App() {
   return (
-    <div className="min-h-screen bg-mobile-light bg-contain bg-no-repeat py-12 px-6">
+    <div className="min-h-screen bg-gray-100 bg-mobile-light bg-contain bg-no-repeat py-12 px-6">
       <Header />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
+
+      <div className="mt-6 mb-4">
+        <Form />
+      </div>
+
+      <ul className="overflow-hidden rounded-lg bg-white shadow-xl">
+        {Todos.map((todo, index) => {
+          return <ListItem key={index} data={todo} />;
+        })}
+        <li className="flex justify-between px-4 py-3 text-xs text-slate-400">
+          <span>5 items left</span>
+          <button>Clear Completed</button>
+        </li>
+      </ul>
+
+      <div className="mt-4 flex justify-center gap-4 rounded-lg bg-white py-4 text-slate-500">
+        <button className="font-semibold text-blue-500">All</button>
+        <button className="font-semibold">Active</button>
+        <button className="font-semibold">Completed</button>
+      </div>
+
+      <footer className="text-slate-400 text-center mt-10">Drag and drop to reorder list</footer>
     </div>
   );
 }
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,12 +1,14 @@
 import iconCross from '../images/icon-cross.svg';
 
-type Props = {};
+type Props = {
+  data: { title: string; isCompleted: boolean };
+};
 
-const ListItem = ({}: Props) => {
+const ListItem = ({ data }: Props) => {
   return (
     <li className="flex items-center gap-4 border-b border-b-gray-300 bg-white px-4 py-3">
       <button className="h-5 w-5 rounded-full border border-gray-300"></button>
-      <span className="mt-1 flex-1 text-sm text-slate-600">Create a new todo...</span>
+      <span className="mt-1 flex-1 text-sm text-slate-600">{data.title}</span>
       <button className="h-3 w-3">
         <img src={iconCross} alt="delete task" />
       </button>
